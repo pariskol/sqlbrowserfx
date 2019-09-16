5c7570fe1f3a64893a550b8f2eef0250fee2a788
Paris Kolovos <pariskolovos@live.com>
2019-09-17 00:21:23 +0300

    add starters for linux, windows

---
+++ starters/sqlbrowserfx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+main_jar=$(find . -name "sql-browser*")
+main_class=gr.sqlbrowserfx.SqlBrowserFXApp
+
+$JAVA_HOME/java -Xmx512m -Xms128m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 \
+-Dlog4j.configuration=file:./log4j.properties \
+-cp lib/*:glib/*:$main_jar $main_class
