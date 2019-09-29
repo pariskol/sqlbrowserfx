10ade3bb910aa1217b518b0f92a131c712c79957
Paris Kolovos <pariskolovos@live.com>
2019-09-29 16:44:56 +0300

    transfer data with changed method

--- src/main/java/gr/sqlbrowserfx/listeners/SimpleObservable.java
+++ src/main/java/gr/sqlbrowserfx/listeners/SimpleObservable.java
@@ -3,6 +3,7 @@ package gr.sqlbrowserfx.listeners;
 public interface SimpleObservable<T> {
 
 	public void changed();
+	public void changed(T data);
 	public void addListener(SimpleChangeListener<T> listener);
 	public void removeListener(SimpleChangeListener<T> listener);
 }
