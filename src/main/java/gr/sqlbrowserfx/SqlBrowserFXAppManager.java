7f4c2b7b9bddd518a3ae73da14b1bb41afe02f4b
Paris Kolovos <pariskolovos@live.com>
2019-11-09 00:00:52 +0200

    remove unused code

--- src/main/java/gr/sqlbrowserfx/SqlBrowserFXAppManager.java
+++ src/main/java/gr/sqlbrowserfx/SqlBrowserFXAppManager.java
@@ -6,14 +6,12 @@ import java.util.List;
 import gr.sqlbrowserfx.conn.SqlConnector;
 import gr.sqlbrowserfx.conn.SqliteConnector;
 import gr.sqlbrowserfx.dock.nodes.DSqlPane;
-import gr.sqlbrowserfx.nodes.sqlCodeArea.SqlCodeArea;
 import gr.sqlbrowserfx.nodes.sqlPane.SqlPane;
 
 public class SqlBrowserFXAppManager {
 
 	private static SqlConnector sqlConnector = new SqliteConnector("./sqlbrowser.db");
 	private static List<DSqlPane> sqlPanesList = new ArrayList<>();
-	private static List<SqlCodeArea> sqlCodeAreasList;
 	
 	public static SqlConnector getConfigSqlConnector() {
 		return sqlConnector;
@@ -23,18 +21,10 @@ public class SqlBrowserFXAppManager {
 		sqlPanesList.add(sqlPane);
 	}
 	
-	public static void addSqlCodeArea(SqlCodeArea codeArea) {
-		sqlCodeAreasList.add(codeArea);
-	}
-	
 	public static List<DSqlPane> getActiveSqlPanes() {
 		return sqlPanesList;
 	}
 	
-	public static List<SqlCodeArea> getActiveSqlCodeAreas() {
-		return sqlCodeAreasList;
-	}
-
 	public static void removeSqlPane(SqlPane sqlPane) {
 		sqlPanesList.remove(sqlPane);
 	}
