0e6642e00e619fd58daa01920b14d16fe90a5809
Paris Kolovos <pariskolovos@live.com>
2019-11-30 13:28:58 +0200

    create class DockWeights for readability

---
+++ src/main/java/gr/sqlbrowserfx/dock/DockWeights.java
@@ -0,0 +1,14 @@
+package gr.sqlbrowserfx.dock;
+
+public class DockWeights {
+
+	/**
+	 * Method for code readability apply weights to dock nodes in same order.
+	 * 
+	 * @param weights
+	 * @return double[] array
+	 */
+	public static double[] asDoubleArrray(double... weights) {
+		return weights;
+	}
+}
