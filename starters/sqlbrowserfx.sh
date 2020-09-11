7f30794c27921bcb03f3f5d21672c64f45631d8b
Paris Kolovos <pariskolovos@live.com>
2020-09-12 02:42:42 +0300

    remove useless starter

--- starters/sqlbrowserfx.sh
+++ starters/sqlbrowserfx.sh
@@ -2,9 +2,9 @@
 
 cd "$(dirname "$0")"
 
-#JAVA_HOME
 main_jar=$(find . -name "sqlbrowser*.jar")
-main_class=gr.sqlbrowserfx.SqlBrowserFXApp
+main_class=gr.sqlbrowserfx.GUIStarter
 
-$JAVA_HOME/java -Xmx512m -Xms128m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 \
--cp lib/*:glib/*:$main_jar $main_class $1
+java -Xmx512m -Xms128m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 \
+-Dlog4j.configuration=file:./log4j.properties \
+-cp lib/*:$main_jar $main_class
