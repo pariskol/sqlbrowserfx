67af269d1f1ec68fa30672cd49cac1f48cd0c7a0
Paris Kolovos <pariskolovos@live.com>
2021-03-01 00:41:41 +0200

    create font scaling utility for javafx nodes

--- src/main/java/gr/sqlbrowserfx/utils/JavaFXUtils.java
+++ src/main/java/gr/sqlbrowserfx/utils/JavaFXUtils.java
@@ -1,9 +1,16 @@
 package gr.sqlbrowserfx.utils;
 
+import org.fxmisc.wellbehaved.event.EventPattern;
+import org.fxmisc.wellbehaved.event.InputMap;
+import org.fxmisc.wellbehaved.event.Nodes;
+
+import javafx.event.Event;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.input.ScrollEvent;
 import jfxtras.styles.jmetro.JMetro;
 import jfxtras.styles.jmetro.Style;
@@ -11,6 +18,7 @@ import jfxtras.styles.jmetro.Style;
 public class JavaFXUtils {
 
 	private static  String CSS_THEME = "/styles/" + (String) PropertiesLoader.getProperty("sqlbrowserfx.css.theme", String.class, "flat-dark");
+	private static double ZOOM = 1.0;
 	private static final boolean ENABLE_JMETRO =  PropertiesLoader.getProperty("sqlbrowserfx.jmetro.theme", String.class) != null;
 	private static final String JMETRO = (String) PropertiesLoader.getProperty("sqlbrowserfx.jmetro.theme", String.class);
 
@@ -39,7 +47,7 @@ public class JavaFXUtils {
 	
 	public static void addMouseScrolling(Node node) {
         node.setOnScroll((ScrollEvent event) -> {
-            // Adjust the zoom factor as per your requirement
+            // Adjust the ZOOM factor as per your requirement
         	if (event.isControlDown()) {
 	            double zoomFactor = 1.05;
 	            double deltaY = event.getDeltaY();
@@ -51,6 +59,29 @@ public class JavaFXUtils {
 	            event.consume();
         	}
         });
+	}
+	
+	public static void addZoomInOutSupport(Node node) {
+		InputMap<Event> zoomIn = InputMap.consume(
+				EventPattern.keyPressed(KeyCode.EQUALS, KeyCombination.CONTROL_DOWN),
+				action -> {
+					if (ZOOM < 2.0)
+						ZOOM += 0.1;
+					node.setStyle("-fx-font-size: " + ZOOM + "em;");
+				}
+		);
+		InputMap<Event> zoomOut = InputMap.consume(
+				EventPattern.keyPressed(KeyCode.MINUS, KeyCombination.CONTROL_DOWN),
+				action -> {
+					if (ZOOM > 0.8)
+						ZOOM -= 0.1;
+					node.setStyle("-fx-font-size: " + ZOOM + "em;");
+				}
+		);
+		
+        Nodes.addInputMap(node, zoomIn);
+        Nodes.addInputMap(node, zoomOut);
+        
 	}
 	
 	public static void applyJMetro(Node node) {
