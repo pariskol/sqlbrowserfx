340ff7be04c290023f74fc287908469477f5db9b
Paris Kolovos <pariskolovos@live.com>
2022-01-23 20:42:18 +0200

    update properties and scripts

--- create-jre.sh
+++ create-jre.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-modules="java.base,java.datatransfer,java.desktop,jdk.unsupported,java.sql"
-jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules java.base,java.datatransfer,java.desktop,java.logging,jdk.unsupported --output jre
+modules="java.base,java.datatransfer,java.desktop,jdk.unsupported,java.logging,java.sql"
+jlink --no-header-files --no-man-pages --compress=2 --strip-debug --add-modules $modules --output jre
