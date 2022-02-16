dd5fd9b15ce9180b29b6b38792354ae3e4fad75d
Paris_Kolovos <pariskolovos@live.com>
2022-02-16 22:19:41 +0200

    load prop for mode eith PropertiesLoader

--- src/main/java/gr/sqlbrowserfx/GUIStarter.java
+++ src/main/java/gr/sqlbrowserfx/GUIStarter.java
@@ -1,9 +1,11 @@
 package gr.sqlbrowserfx;
 
+import gr.sqlbrowserfx.utils.PropertiesLoader;
+
 public class GUIStarter {
 
 	public static void main(String[] args) {
-		if (System.getProperty("sqlbrowserfx.mode", "advanced").equals("advanced"))
+		if (PropertiesLoader.getProperty("sqlbrowserfx.mode", String.class, "advanced").equals("advanced"))
 			SqlBrowserFXApp.main(args);
 		else
 			SqlBrowserFXAppWithoutDocking.main(args);
