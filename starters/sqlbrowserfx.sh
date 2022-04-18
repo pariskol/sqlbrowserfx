d97ae7d4622a7a0f6115ab36e2059c1024bf8c1d
Paris_Kolovos <pariskolovos@live.com>
2022-04-18 10:26:41 +0300

    update starter script

--- starters/sqlbrowserfx.sh
+++ starters/sqlbrowserfx.sh
@@ -6,5 +6,5 @@ main_jar=$(find . -name "sqlbrowser*.jar")
 main_class=gr.sqlbrowserfx.GUIStarter
 
 [ ! -z "$JAVA_HOME" ] && JAVA_HOME=$JAVA_HOME/bin/
-${JAVA_HOME}java -Xmx512m -Xms128m -XX:+UseG1GC -XX:MaxGCPauseMillis=100 \
+${JAVA_HOME}java -Xmx512m -Xms128m -XX:+UseG1GC -XX:MaxGCPauseMillis=100  -Dprism.lcdtext=false \
 -cp lib/*:$main_jar $main_class
